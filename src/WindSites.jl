cb9d8ecdd7e13d041b2f20e76dee0d9f00d7268d
Niclas Mattsson <niclas.mattsson@gmail.com>
2020-08-05 19:18:42 +0200

    Read databases and plot turbines on Google Maps

---
+++ src/WindSites.jl
@@ -0,0 +1,82 @@
+module WindSites
+
+using Proj4, XLSX, CSV, DataFrames, Dates
+
+export openmap, readusa, readdk, df_dk, df_usa
+
+function openmap(df::DataFrame, turbinenumber::Int)
+    lon, lat = df[turbinenumber, :lon], df[turbinenumber, :lat]
+    url = "http://maps.google.com/maps?t=k\"&\"q=loc:$lat+$lon"
+    c = Cmd(`cmd /c start \"\" $url`, windows_verbatim=true)
+    run(c)
+    return df[turbinenumber, :]
+end
+
+function openmap(lon::Real, lat::Real)
+    url = "http://maps.google.com/maps?t=k\"&\"q=loc:$lat+$lon"
+    # Extra quotes to avoid errors with special chars ? and &:
+    # https://superuser.com/questions/36728/can-i-launch-urls-from-command-line-in-windows
+    # https://discourse.julialang.org/t/quoting-special-characters-of-a-url-in-cmd-objects-on-windows/44324
+    c = Cmd(`cmd /c start \"\" $url`, windows_verbatim=true)
+    run(c)
+end
+
+function openmapchrome(lon::Real, lat::Real)
+    command = "C:/Program Files (x86)/Google/Chrome/Application/chrome"
+    url = "http://maps.google.com/maps?t=k&q=loc:$lat+$lon"
+    println("Opening $url...")
+    run(`$command $url`)
+end
+
+function readusa()
+    datadir = joinpath(@__DIR__, "..", "data")
+    df = DataFrame!(CSV.File("$datadir/USA_uswtdb_v3_1_20200717.csv"))
+    select!(df, [:t_cap, :xlong, :ylat, :p_year, :t_model, :t_hh, :t_rd])
+    rename!(df, [:capac, :lon, :lat, :year, :model, :hubheight, :rotordiam])
+    delete!(df, ismissing.(df[:, :capac]))
+    delete!(df, ismissing.(df[:, :year]))
+    df[!, :capac] = convert.(Int, df[!, :capac])
+    return df
+end
+
+function readdk()
+    datadir = joinpath(@__DIR__, "..", "data")
+    cols = [3,13,14,2,7,5,4]
+    df = DataFrame!(XLSX.readtable("$datadir/DK_anlaegprodtilnettet_0.xlsx", "IkkeAfmeldte-Existing turbines",
+        "A:P", first_row=19, header=false, infer_eltypes=false)...)[!, cols]
+    rename!(df, [:capac, :lon, :lat, :year, :model, :hubheight, :rotordiam])
+    df[!, :capac] = convert.(Int, round.(df[!, :capac]))
+    df[!, :model] = string.(df[:, :model])
+    df[!, :hubheight] = convert.(Float64, df[!, :hubheight])
+    df[!, :rotordiam] = convert.(Float64, df[!, :rotordiam])
+    df[!, :year] = Dates.year.(df[!, :year])
+    delete!(df, ismissing.(df[:, :lon]))
+
+    x = df[!, :lon]
+    y = df[!, :lat]
+    source = Projection("+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs")    # EPSG:25832
+    dest = Projection("+proj=longlat +datum=WGS84 +no_defs")
+    len = size(df,1)
+    lon, lat = zeros(len), zeros(len)
+    for i = 1:len
+        lon[i], lat[i], _ = Proj4.transform(source, dest, [x[i] y[i] 0])
+    end
+    df[!, :lon] = lon
+    df[!, :lat] = lat
+    return df
+end
+
+df_dk = readdk()
+df_usa = readusa()
+
+# println("\nProjecting coordinates (Mollweide)...")
+# res = 0.01
+# res2 = res/2
+# lons = (-180+res2:res:180-res2)[lonrange]         # longitude values (pixel center)
+# lats = (90-res2:-res:-90+res2)[latrange]          # latitude values (pixel center)
+# source = LonLat()
+# dest = Projection("+proj=moll +lon_0=$(mean(lons))")
+# xs, ys = xygrid(lons, lats)
+# Proj4.transform!(source, dest, vec(xs), vec(ys))
+
+end # module
