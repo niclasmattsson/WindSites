bc985d3d8ffe24397277fe4839f9a024c1644958
Niclas Mattsson <niclas.mattsson@gmail.com>
2020-08-11 16:31:56 +0200

    Add and parse wind park data from UK  REPD

--- src/WindSites.jl
+++ src/WindSites.jl
@@ -2,7 +2,7 @@ module WindSites
 
 using Proj4, XLSX, CSV, DataFrames, Dates
 
-export openmap, readusa, readdk, df_dk, df_usa
+export openmap, readusa, readdk, readuk, df_dk, df_usa, df_uk
 
 function openmap(df::DataFrame, turbinenumber::Int)
     lon, lat = df[turbinenumber, :lon], df[turbinenumber, :lat]
@@ -39,6 +39,39 @@ function readusa()
     return df
 end
 
+parse_not_missing(T, x) = ismissing(x) ? missing : parse(T, x)
+
+function readuk()
+    datadir = joinpath(@__DIR__, "..", "data")
+    cols = [6,9,14,15,16,19,25,26,47]
+    df = DataFrame!(CSV.File("$datadir/UK_REPD-June-2020-update.csv"))[!, cols]
+    rename!(df, [:type, :capac_park, :capac, :nturbines, :height, :status, :lon, :lat, :year])
+    delete!(df, .!startswith.(df[!, :type], "Wind"))
+    delete!(df, df[:, :status] .!= "Operational")
+    select!(df, Not(:status))
+    df[!, :type] = replace.(df[!, :type], "Wind " => "")
+    df[!, :nturbines] = parse.(Int, df[!, :nturbines])
+    df[!, :height] = parse_not_missing.(Int, df[!, :height])
+    df[!, :capac] = parse.(Float64, df[!, :capac])
+    df[!, :capac_park] = parse.(Float64, replace.(df[!, :capac_park], "," => ""))
+    df[!, :lon] = parse.(Int, replace.(df[!, :lon], "," => ""))
+    df[!, :lat] = parse.(Int, replace.(df[!, :lat], "," => ""))
+    df[!, :year] = Date.(df[!, :year], dateformat"d/m/y")
+
+    x = df[!, :lon] # BNG: British National Grid = EPSG:27700
+    y = df[!, :lat] # https://en.wikipedia.org/wiki/Ordnance_Survey_National_Grid#General
+    source = Projection("+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs")
+    dest = Projection("+proj=longlat +datum=WGS84 +no_defs")
+    len = size(df,1)
+    lon, lat = zeros(len), zeros(len)
+    for i = 1:len
+        lon[i], lat[i], _ = Proj4.transform(source, dest, [x[i] y[i] 0])
+    end
+    df[!, :lon] = lon
+    df[!, :lat] = lat
+    return df
+end
+
 function readdk()
     datadir = joinpath(@__DIR__, "..", "data")
     cols = [3,13,14,2,7,5,4,10]
@@ -53,9 +86,9 @@ function readdk()
     df[!, :onshore] = uppercase.(df[!, :onshore]) .== "LAND"    # no missings here, no need to check
     delete!(df, ismissing.(df[:, :lon]))
 
-    x = df[!, :lon]
-    y = df[!, :lat]
-    source = Projection("+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs")    # EPSG:25832
+    x = df[!, :lon] # European Terrestrial Reference System 1989: ETRS89/UTM zone 32N = EPSG:25832
+    y = df[!, :lat] # https://en.wikipedia.org/wiki/European_Terrestrial_Reference_System_1989
+    source = Projection("+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
     dest = Projection("+proj=longlat +datum=WGS84 +no_defs")
     len = size(df,1)
     lon, lat = zeros(len), zeros(len)
@@ -67,11 +100,15 @@ function readdk()
     return df
 end
 
-df_dk = readdk()
-df_usa = readusa()
 
-CSV.write("turbines_DK.csv", df_dk)
-CSV.write("turbines_USA.csv", df_usa)
+
+# df_dk = readdk()
+# df_usa = readusa()
+# df_uk = readuk()
+
+# CSV.write("turbines_DK.csv", df_dk)
+# CSV.write("turbines_USA.csv", df_usa)
+
 
 # println("\nProjecting coordinates (Mollweide)...")
 # res = 0.01
