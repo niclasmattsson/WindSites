6b5bfedda46eb0ca5e96f3a1afd08c3993c8199d
Niclas Mattsson <niclas.mattsson@gmail.com>
2020-10-02 11:24:04 +0200

    Tweak turbine scatter chart, add lines to grid neighbors

--- src/WindSites.jl
+++ src/WindSites.jl
@@ -1,6 +1,7 @@
 module WindSites
 
-using Proj4, XLSX, CSV, DataFrames, Dates, GDAL_jll, Pkg.TOML, Plots, Loess, SmoothingSplines
+using Proj4, XLSX, CSV, DataFrames, Dates, GDAL_jll, Pkg.TOML, Plots, MAT,
+        Loess, SmoothingSplines
 
 export openmap, readusa, readdk, readuk, readse, readde, readturbinedata, shapefile2csv,
     scatterplots_model, plotdist, fill_missing_rotordiams!
@@ -289,7 +290,7 @@ function fill_missing_rotordiams!(df_dk, df_usa, df_uk, df_se, df_de; rotordiamp
     end
 end
 
-function scatterplots_model(gisregion, type=[:total])
+function scatterplots_model(gisregion, type=[:total]; showlines=false)
     dfg = DataFrame!(CSV.File(in_datafolder("output", "regionalwindGIS_$gisregion.csv")))
     dfm = DataFrame!(CSV.File(in_datafolder("output", "windresults_$gisregion.csv")))
     dfm[!,:mcap] = vec(sum(Array(dfm[:, [:cap1, :cap2, :cap3, :cap4, :cap5]]), dims=2))
@@ -301,24 +302,42 @@ function scatterplots_model(gisregion, type=[:total])
     dfm[:moffclass] = (1*dfm[:offcap1] + 2*dfm[:offcap2] + 3*dfm[:offcap3] + 4*dfm[:offcap4] + 5*dfm[:offcap5]) ./ dfm[:moffcap]
     dfm[:moffclass] = replace(round.(dfm[:moffclass], digits=2), NaN => missing)
 
+    distancevars = matread(in_datafolder("output", "distances_$gisregion.mat"))
+    connectedregions = findall(distancevars["connected"] .| distancevars["connectedoffshore"])
+    connected = [c for c in connectedregions if c[1] < c[2]]
+
     df = innerjoin(dfg, dfm, on=:region)
-    select!(df, [:region, :capac, :offcapac, :elec2018, :mcap, :moffcap, :melec, :moffelec, :mclass, :moffclass])
+    select!(df, [:region, :capac, :offcapac, :elec2018, :class, :mcap, :moffcap,
+                    :melec, :moffelec, :mclass, :moffclass, :exploit_tot, :masked])
     plotly()
     println(type)
     if :onshore in type
         s = scatter(df[:capac], df[:mcap], markersize=df[:mclass]*2, title="Onshore capacity",
-            hover=df[:region].*" class ".*string.(df[:mclass]),
-            xlabel="GW (real)", ylabel="GW (model)", size=(800,650))
+            hover=df[:region].*" class ".*string.(df[:mclass]), legend=false, colorbar=true,
+            xlabel="GW (real)", ylabel="GW (model)", size=(800,550))
         display(s)
     elseif :offshore in type
         s = scatter(df[:offcapac], df[:moffcap], markersize=df[:moffclass]*2, title="Offshore capacity",
-            hover=df[:region].*" class ".*string.(df[:moffclass]),
-            xlabel="GW (real)", ylabel="GW (model)", size=(800,650))
+            hover=df[:region].*" class ".*string.(df[:moffclass]), legend=false, colorbar=true,
+            xlabel="GW (real)", ylabel="GW (model)", size=(800,550))
         display(s)
     elseif :total in type
-        s = scatter(df[:capac]+df[:offcapac], df[:mcap]+df[:moffcap], markersize=df[:mclass]*2, title="Total capacity",
-            hover=df[:region].*" class ".*string.(df[:mclass]),
-            xlabel="GW (real)", ylabel="GW (model)", size=(800,650))
+        x = df[:capac] + df[:offcapac]
+        y = df[:mcap] + df[:moffcap]
+        s = plot()
+        xx = [x[c[d]] for d in 1:2, c in connected]
+        yy = [y[c[d]] for d in 1:2, c in connected]
+        showlines && plot!(xx, yy, c=WindSites.RGB(.85,.85,.85), legend=:none)
+        scatter!(x, y, markersize=df[:class]*2, title="Total capacity", c=1,
+            hover=df[:region].*"<br>real onshore = ".*string.(df[:capac]).*" MW".*
+            "<br>real offshore = ".*string.(df[:offcapac]).*" MW".*
+            "<br>exploited onshore = ".*string.(df[:exploit_tot]).*"%".*
+            "<br>class = ".*string.(df[:class]).*
+            "<br>m_onshore = ".*string.(df[:mcap]).*" GW<br>m_offshore = ".*string.(df[:moffcap]).*
+            " GW<br>m_class = ".*string.(df[:mclass]).*
+            "<br>m_offclass = ".*string.(df[:moffclass]).*"<br>masked onshore = ".*string.(df[:masked]),
+            legend=false, colorbar=true, marker_z=df[:masked], color=:watermelon, 
+            xlabel="GW (real)", ylabel="GW (model)", size=(800,550))
         display(s)
     elseif :elec in type
         s = scatter(df[:elec2018], df[:melec], markersize=df[:mclass], xlabel="GWh (real)", ylabel="GWh (model)")
